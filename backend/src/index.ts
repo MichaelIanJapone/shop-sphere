2acbf8a1cb4b3b8e29135df619579db1c23f670a
Michael Ian Japone <michaelianjapone@gmail.com>
2026-08-28 13:43:05 +0800

    Add polar webhook handler and integrate with existing routes

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -12,6 +12,7 @@ import meRouter from "./routes/meRouter";
 import productRouter from "./routes/productRouter";
 import streamRouter from "./routes/streamRouter";
 import checkoutRouter from "./routes/checkoutRouter";
+import { polarWebhookHandler } from "./webhooks/polar";
 
 const env = getEnv();
 const app = express();
@@ -22,9 +23,9 @@ app.post("/webhooks/clerk", rawJson, (req, res) => {
   void clerkWebhookHandler(req, res);
 });
 
-//app.post("/webhooks/polar", rawJson, (req, res) => {
-//  void polarWebhookHandler(req, res);
-//});
+app.post("/webhooks/polar", rawJson, (req, res) => {
+  void polarWebhookHandler(req, res);
+});
 
 app.use(express.json());
 app.use(cors());
@@ -63,4 +64,3 @@ app.listen(env.PORT, () => {
     keepAliveCron.start();
     }
 });
-
